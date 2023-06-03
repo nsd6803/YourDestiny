d908f907e77045f892bd57380f5ee15babe50055
nsd6803 <nsd6803@mail.ru>
2023-06-03 18:51:42 +0300

    Almost Done

--- app/src/main/java/com/example/yourdestiny/model/profileDB/ProfileDao.java
+++ app/src/main/java/com/example/yourdestiny/model/profileDB/ProfileDao.java
@@ -18,13 +18,13 @@ public interface ProfileDao {
     @Query("SELECT * FROM Profile WHERE name LIKE :name")
     Profile getAll(String name);
 
-    @Query("SELECT name FROM Profile WHERE uid LIKE 1")
+    @Query("SELECT name FROM Profile LIMIT 1")
     String getName();
 
-    @Query("SELECT mail FROM Profile WHERE uid LIKE 1")
+    @Query("SELECT mail FROM Profile LIMIT 1")
     String getMail();
 
-    @Query("SELECT password FROM Profile WHERE uid LIKE 1")
+    @Query("SELECT password FROM Profile LIMIT 1")
     String getPassword();
 
     @Query("DELETE FROM Profile")
